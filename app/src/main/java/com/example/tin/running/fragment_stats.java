d6a800db0d7a566a0bf425b2d28fd0f997f5531a
martincolts <jmartinlopez89@gmail.com>
2015-08-23 13:28:36 -0300

    AvSpeed

--- app/src/main/java/com/example/tin/running/fragment_stats.java
+++ app/src/main/java/com/example/tin/running/fragment_stats.java
@@ -234,8 +234,9 @@ public class fragment_stats extends Fragment {
                     Double currentSpeed2 = MainActivity.mGPSService.getCurrentSpeed() * (3.6);
                     currentSpeed.setText(new DecimalFormat("#.#").format(currentSpeed2) + " Km/h");
 
-                    Double avSpeed = (Double)(distance2 / MainActivity.mChronometerService.getSeconds()*(10/36));
+                    Double avSpeed = (Double)(distance2 / MainActivity.mChronometerService.getSeconds()*3.6);
                     String a = avSpeed.toString();
+                    a += "Km/h";
                     //avSpeedData.setText(new DecimalFormat("#.#").format(avSpeed) + " Km/h");i
                     avSpeedData.setText (a);
 
